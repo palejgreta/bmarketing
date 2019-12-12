b8a31be46199b16a2ff8fdacbbd8b6ec1d33b7d0
[Peter Fellner] <[peter_fellner@hotmail.com]>
2019-12-12 14:33:18 +0000

    maybe ready ;)

--- clean_mater.R
+++ clean_mater.R
@@ -2,10 +2,15 @@ data_cleaner<- function(){
   
   
   # Fellner: return an error if the target variable contains any missing values (NA’s).
-  target_clean<- function(input_dat, target_col){
+  target_clean <- function(input_dat, target_col="y"){
     # input_dat=bmarketing
-    
+    if(length(which(is.na(input_dat$target_col)==TRUE)>0)){
+      stop("Missing Value found in the specified column")
+    } else{
+      return("All okay: No Missing Value found in the specified column")
+    }
   }
+
   #Weber: Give clear warnings for all other variables which contain NA’s.
   other_clean <-function(input_dat, col_skip){
     # maybe a for cycle?
