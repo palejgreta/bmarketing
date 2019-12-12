27f6a8cee43fbff3ec74bb68d36b4d1969ebf2e6
[ChristophAT] <[christoph.weber@uniqa.at]>
2019-12-12 15:06:54 +0000

    [other_clean function]

--- clean_mater.R
+++ clean_mater.R
@@ -12,10 +12,24 @@ data_cleaner<- function(){
   }
 
   #Weber: Give clear warnings for all other variables which contain NA’s.
-  other_clean <-function(input_dat, col_skip){
+  other_clean <- function(input_dat, col_skip = 0){
     # maybe a for cycle?
-    
+    for(j in 1:ncol(input_dat)) {
+      if(j!=col_skip) {
+        for(i in 1:nrow(input_dat)) {
+          #print(input_dat[i,j])
+          if(is.na(input_dat[i,j])==TRUE){
+            cat("NA in Column ", j,"\n")
+            i <- nrow(input_dat)+1
+          }
+        }
+      }
+    }
   }
+  
+  #test data.frame
+  #test <- data.frame(c(NA, "test"), c(NA,1),c(1,2))
+  #other_clean(test,3) 
   
   #Kopaczynski: Remove any columns (and report as warning) which contain more than 50% NA’s.
   col_del<-function(input_dat, target_col){
