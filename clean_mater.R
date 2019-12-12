10f445d73f7a0974bdab83f925adfe2226c4a227
Greta Palej <quantargo@aff673b1f46d>
2019-12-12 13:52:49 +0000

    Just push it :)

---
+++ clean_mater.R
@@ -0,0 +1,20 @@
+data_cleaner<- function(){
+  
+  
+  # Fellner: return an error if the target variable contains any missing values (NA’s).
+  target_clean<- function(input_dat, target_col){
+    # input_dat=bmarketing
+    
+  }
+  #Weber: Give clear warnings for all other variables which contain NA’s.
+  other_clean <-function(input_dat, col_skip){
+    # maybe a for cycle?
+    
+  }
+  
+  #Kopaczynski: Remove any columns (and report as warning) which contain more than 50% NA’s.
+  col_del<-function(input_dat, target_col){
+    
+  }
+  
+}
