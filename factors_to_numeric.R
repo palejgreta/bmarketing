17a0b7f176b4a1c60175ec4de8a1fdd67fbf1f38
[ChristophAT] <[christoph.weber@uniqa.at]>
2019-12-13 09:15:50 +0000

    re-order levels

--- factors_to_numeric.R
+++ factors_to_numeric.R
@@ -1,5 +1,13 @@
 # Weber : check factor levels
 factor_to_num<- function(input_data){
+  stopifnot(("day_of_week" %in% colnames(bmarketing)) || ("month" %in% colnames(input_data)))
+
+  levels(input_data$day_of_week) <- c("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+  levels(input_data$month) <- c("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec")
   
+  #reorder(input_data$day_of_week, c("mon", "tue", "wed", "thu", "fri", "sat", "sun"))
+  #reorder(input_data$month, c("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"))
   
+  input_data
 }
+
