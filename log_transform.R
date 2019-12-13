9a40356daed98822543bc7d3612b461da3e9371f
[Peter Fellner] <[peter_fellner@hotmail.com]>
2019-12-13 09:22:12 +0000

    log transformation ready

--- log_transform.R
+++ log_transform.R
@@ -2,6 +2,11 @@
 
 #name of the sript: log_transform.R
 log_fun<- function(input_data){
-  
-  
+  for (i in 1: ncol(input_data)){ 
+    if(is.numeric(input_data[,i]) && min(input_data[,i])>0){
+        input_data[,i] = log(input_data[,i])
+    }
+  } 
+  input_data
 }
+
